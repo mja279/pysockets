f05a1379f6501400dc4f7ec395079d065b59e230
Maria Jimena <mja279@georgetown.edu>
2021-07-23 21:57:15 -0400

    initial commit

---
+++ browserwebsock.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+# WS server that sends messages at random intervals
+
+import asyncio
+import datetime
+import random
+import websockets
+import requests
+
+async def time(websocket, path):
+    while True:
+        now = datetime.datetime.utcnow().isoformat() + "Z"
+        await websocket.send(now)
+        await asyncio.sleep(random.random() * 3)
+
+
+async def hello(websocket, path):
+    name = await websocket.recv()
+    print(f"< {name}")
+
+    greeting = f"Hello {name}!"
+
+    await websocket.send(greeting)
+    print(f"> {greeting}")
+
+
+
+
+start_server = websockets.serve(hello, "127.0.0.1", 5678)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
