0b82ccb065048a9b44897713147ef0c6509c7fe9
sarrazol <sebastianarrazola@gmail.com>
2021-08-11 20:30:50 -0400

    added sampleUI2

--- browserwebsock.py
+++ browserwebsock.py
@@ -9,9 +9,9 @@ import websockets
 import requests
 import json
 
-import cv2
-import mss
-import numpy
+# import cv2
+# import mss
+# import numpy
 
 CLIENTS = set()
 MESSAGES = []
@@ -164,35 +164,35 @@ async def goodbye(websocket, path):
     #print(f"> {greeting}")
     # await asyncio.sleep(1)
 
-async def shareScreen(websocket, path):
-    import time
-    CLIENTS.add(websocket)
-    dumbassBool = True;
-    streamStart = await websocket.recv()
-    print("STREAM STARTING")
-    with mss.mss() as sct:
-        # Part of the screen to capture
-        monitor = {"top": 40, "left": 0, "width": 800, "height": 640}
+# async def shareScreen(websocket, path):
+#     import time
+#     CLIENTS.add(websocket)
+#     dumbassBool = True;
+#     streamStart = await websocket.recv()
+#     print("STREAM STARTING")
+#     with mss.mss() as sct:
+#         # Part of the screen to capture
+#         monitor = {"top": 40, "left": 0, "width": 800, "height": 640}
   
-        while "Screen capturing":
-            last_time = time.time()
+#         while "Screen capturing":
+#             last_time = time.time()
   
-            # Get raw pixels from the screen, save it to a Numpy array
-            img = numpy.array(sct.grab(monitor))
+#             # Get raw pixels from the screen, save it to a Numpy array
+#             img = numpy.array(sct.grab(monitor))
   
-            # Display the picture
-            cv2.imshow("OpenCV/Numpy normal", img)
+#             # Display the picture
+#             cv2.imshow("OpenCV/Numpy normal", img)
   
-            # Display the picture in grayscale
-            # cv2.imshow('OpenCV/Numpy grayscale',
-            #            cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY))
+#             # Display the picture in grayscale
+#             # cv2.imshow('OpenCV/Numpy grayscale',
+#             #            cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY))
   
-            print("fps: {}".format(1 / (time.time() - last_time)))
+#             print("fps: {}".format(1 / (time.time() - last_time)))
   
-            # Press "q" to quit
-            if cv2.waitKey(25) & 0xFF == ord("q"):
-                cv2.destroyAllWindows()
-                break
+#             # Press "q" to quit
+#             if cv2.waitKey(25) & 0xFF == ord("q"):
+#                 cv2.destroyAllWindows()
+#                 break
 
 # start_server = websockets.serve(shareScreen, "127.0.0.1", 5678)
 # start_server = websockets.serve(hello, "127.0.0.1", 5678)
